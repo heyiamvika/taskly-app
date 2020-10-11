9db25d45a247c77bbfb326777d2998b94145ff62
Viktoriia Nosova <viktoriian@wix.com>
2020-10-11 19:23:28 +0300

    Added methods for user authentification

--- src/components/Firebase/firebase.ts
+++ src/components/Firebase/firebase.ts
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 const config = {
 	apiKey: process.env.REACT_APP_API_KEY,
@@ -10,9 +11,33 @@ const config = {
 };
 
 class Firebase {
+	// TO_DO: search for a correct solution for that (what type should auth be?)
+	auth: any;
+
 	constructor() {
 		app.initializeApp(config);
+		this.auth = app.auth();
 	}
+
+	createUserWithEmailAndPassword = (email: string, password: string) => {
+		this.auth.createUserWithEmailAndPassword(email, password);
+	};
+
+	signInWithEmailAndPassword = (email: string, password: string) => {
+		this.auth.signInWithEmailAndPassword(email, password);
+	};
+
+	signOut = () => {
+		this.auth.signOut();
+	};
+
+	resetPassword = (email: string) => {
+		this.auth.sendPasswordResetEmail(email);
+	};
+
+	updatePassword = (password: string) => {
+		this.auth.currentUser.updatePassword(password);
+	};
 }
 
 export default Firebase;
