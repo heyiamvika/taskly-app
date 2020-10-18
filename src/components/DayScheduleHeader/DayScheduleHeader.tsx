5f388cc0a536a2dad4b7c3b5223a49ebefa62163
Viktoriia Nosova <viktoriian@wix.com>
2020-10-18 20:48:36 +0300

    Added button to add new task to DayScheduleHeader

--- src/components/DayScheduleHeader/DayScheduleHeader.tsx
+++ src/components/DayScheduleHeader/DayScheduleHeader.tsx
@@ -11,12 +11,15 @@ type Props = {
 export function DayScheduleHeader({ visibleDay }: Props) {
 	return (
 		<div className='day-schedule-header'>
-			<span className='day-schedule-header-heading'>Today's schedule</span>
-			<div className='day-schedule-header-active-day'>
-				<span className='day-schedule-header-heading active-day-heading'>{`${
-					FULL_DAYS_OF_THE_WEEK[visibleDay.getDay()]
-				} ${visibleDay.getDate()}`}</span>
+			<div className='day-schedule-wrapper'>
+				<span className='day-schedule-header-heading'>Today's schedule</span>
+				<div className='day-schedule-header-active-day'>
+					<span className='day-schedule-header-heading active-day-heading'>{`${
+						FULL_DAYS_OF_THE_WEEK[visibleDay.getDay()]
+					} ${visibleDay.getDate()}`}</span>
+				</div>
 			</div>
+			<button className='add-new-task-btn'>+</button>
 		</div>
 	);
 }
