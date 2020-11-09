d085cf7ebf896290b809a6f97c1a156d13443e3e
Viktoriia Nosova <viktoriian@wix.com>
2020-11-09 16:24:55 +0200

    Refactored calendarEvents using createSlice

--- src/store/calendarEvents.ts
+++ src/store/calendarEvents.ts
@@ -1,64 +1,59 @@
 import { Store, Calendar, CalendarEvent } from './storeTypes';
-import { createAction, createReducer } from '@reduxjs/toolkit';
-
-// Action creators (Redux DevTools);
-export const newEventAdded = createAction<{
-	eventDate: string;
-	newEvent: CalendarEvent;
-}>('NEW_EVENT_ADDED');
-
-export const eventDeleted = createAction<{
-	eventDate: string;
-	id: number;
-}>('EVENT_DELETED');
-
-export const eventDetailsChanged = createAction<{
-	eventDate: string;
-	id: number;
-	changedEvent: CalendarEvent;
-}>('EVENT_DETAILS_CHANGED');
+import { createSlice } from '@reduxjs/toolkit';
 
 // temporary
 let nextId = 0;
 
-const initialStore: Store = {
+const initialState: Store = {
 	currentUser: null,
 	calendar: {},
 };
 
-export default createReducer(initialStore, {
-	[newEventAdded.type]: (state, action) => {
-		const { calendar } = state;
-		const { eventDate, newEvent } = action.payload;
-		const eventToAdd = { ...newEvent, id: ++nextId };
+const slice = createSlice({
+	name: 'app',
+	initialState,
+	reducers: {
+		newEventAdded: (state, action) => {
+			const { calendar } = state;
+			const { eventDate, newEvent } = action.payload;
+			const eventToAdd = { ...newEvent, id: ++nextId };
 
-		if (!hasDateInCalendar(calendar, eventDate)) {
-			calendar[eventDate] = [];
-		}
+			if (!hasDateInCalendar(calendar, eventDate)) {
+				calendar[eventDate] = [];
+			}
 
-		calendar[eventDate].push(eventToAdd);
-	},
-	[eventDeleted.type]: (state, action) => {
-		const { calendar } = state;
-		const { eventDate, id } = action.payload;
+			calendar[eventDate].push(eventToAdd);
+		},
+		eventDeleted: (state, action) => {
+			const { calendar } = state;
+			const { eventDate, id } = action.payload;
 
-		calendar[eventDate] = calendar[eventDate].filter(
-			(event) => event.id !== id,
-		);
+			calendar[eventDate] = calendar[eventDate].filter(
+				(event) => event.id !== id,
+			);
 
-		if (isDayEmpty(calendar[eventDate])) {
-			delete calendar[eventDate];
-		}
-	},
-	[eventDetailsChanged.type]: (state, action) => {
-		const { calendar } = state;
-		const { eventDate, id, changedEvent } = action.payload;
+			if (isDayEmpty(calendar[eventDate])) {
+				delete calendar[eventDate];
+			}
+		},
+		eventDetailsChanged: (state, action) => {
+			const { calendar } = state;
+			const { eventDate, id, changedEvent } = action.payload;
+			const eventToChange = { ...changedEvent, id };
 
-		calendar[eventDate] = calendar[eventDate].map((event) =>
-			event.id === id ? changedEvent : event,
-		);
+			calendar[eventDate] = calendar[eventDate].map((event) =>
+				event.id === id ? eventToChange : event,
+			);
+		},
 	},
 });
+
+export const {
+	newEventAdded,
+	eventDeleted,
+	eventDetailsChanged,
+} = slice.actions;
+export default slice.reducer;
 
 // helper functions
 const isDayEmpty = (dayEvents: CalendarEvent[]): boolean =>
